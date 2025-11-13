21715bdd67e6222c49dc3a27d41fe93546881db8
Trey Gonzalez <lucius.gonzalez16@gmail.com>
2025-11-13 10:03:01 -0600

    Get rid of old test MVC's

--- LeaveManagementSystem/Models/TestViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LeaveManagementSystem.Models
-{
-    public class TestViewModel
-    {
-        public required string Name { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-    }
-}
