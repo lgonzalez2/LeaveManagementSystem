e7cac2d9bfcdf468e216763e8c80fb41029204bc
Trey Gonzalez <lucius.gonzalez16@gmail.com>
2025-11-14 09:41:30 -0600

    Add in post Register action to actuall create a record in the user database

--- LeaveManagementSystem/Controllers/AccountController.cs
+++ LeaveManagementSystem/Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LeaveManagementSystem.Models.Account;
+using LeaveManagementSystem.Models.LeaveTypes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LeaveManagementSystem.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -12,6 +24,34 @@ namespace LeaveManagementSystem.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    NormalizedUserName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Login));
+                } else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
         }
 
         public IActionResult VerifyEmail()
